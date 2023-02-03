7b631ffa134ca3b690f2fb2dd947afa773080ba8
Mariana Guzmán <mggzmn91@gmail.com>
2023-02-03 15:52:55 -0600

    Big Calendar events

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -5,6 +5,7 @@ import { addHours } from 'date-fns';
 
 import { CalendarEvent, Navbar } from '../';
 import { localizer, getMessagesEs } from '../../helpers';
+import { useState } from 'react';
 
 const events = [
   {
@@ -20,13 +21,19 @@ const events = [
   },
 ];
 export const CalendarPage = (props) => {
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
   const eventStyleGetter = (event, start, end, isSelected) => {
     const style = {
       backgroundColor: '#CCCCFF',
-    }
-    return{
-      style
-    }
+    };
+    return {
+      style,
+    };
+  };
+  const onDoubleClick = (event) => {};
+  const onSelect = (event) => {};
+  const onViewChanged = (event) => {
+    localStorage.setItem('lastView', event);
   };
   return (
     <>
@@ -35,14 +42,18 @@ export const CalendarPage = (props) => {
         culture='es'
         localizer={localizer}
         events={events}
+        defaultView={lastView}
         startAccessor='start'
         endAccessor='end'
         style={{ height: 'calc(100vh - 80px)' }}
         messages={getMessagesEs()}
         eventPropGetter={eventStyleGetter}
         components={{
-          event: CalendarEvent
+          event: CalendarEvent,
         }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
       />
     </>
   );

e5ae86dd9db3a4048a5dbec3dce4f32826f48e1f
Mariana Guzmán <mggzmn91@gmail.com>
2023-02-03 15:04:24 -0600

    Big Calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,46 @@
-import { Navbar } from "../"
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-export const CalendarPage = () => {
+import { addHours } from 'date-fns';
+
+import { Navbar } from '../';
+import { localizer, getMessagesEs } from '../../helpers';
+
+const events = [
+  {
+    title: 'Mi cumpleaños',
+    notes: '',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+      _id: '123',
+      name: 'Mariana',
+    },
+  },
+];
+export const CalendarPage = (props) => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: '#CCCCFF',
+    }
+    return{
+      style
+    }
+  };
   return (
     <>
-    <Navbar />
+      <Navbar />
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={events}
+        startAccessor='start'
+        endAccessor='end'
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessagesEs()}
+        eventPropGetter={eventStyleGetter}
+      />
     </>
-  )
-}
+  );
+};

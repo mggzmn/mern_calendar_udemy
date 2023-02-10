4b6dae0e03eeb4ad0d646653bee258c7ec7146c0
Mariana Guzmán <mggzmn91@gmail.com>
2023-02-10 12:38:46 -0600

    422 create event and save backend

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,32 +1,35 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { calendarApi } from '../api';
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from '../store';
 
 
 export const useCalendarStore = () => {
-  
-    const dispatch = useDispatch();
-    const { events, activeEvent } = useSelector( state => state.calendar );
 
-    const setActiveEvent = ( calendarEvent ) => {
-        dispatch( onSetActiveEvent( calendarEvent ) )
+    const dispatch = useDispatch();
+    const { events, activeEvent } = useSelector(state => state.calendar);
+    const { user } = useSelector(state => state.auth);
+
+    const setActiveEvent = (calendarEvent) => {
+        dispatch(onSetActiveEvent(calendarEvent))
     }
 
-    const startSavingEvent = async( calendarEvent ) => {
-        
-        if( calendarEvent._id ) {
+    const startSavingEvent = async (calendarEvent) => {
+
+        if (calendarEvent._id) {
             // Actualizando
-            dispatch( onUpdateEvent({ ...calendarEvent }) );
+            dispatch(onUpdateEvent({ ...calendarEvent }));
         } else {
             // Creando
-            dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }) );
+            const { data } = await calendarApi.post('/events', calendarEvent);
+            dispatch(onAddNewEvent({ ...calendarEvent, id: data.evento.id, user }));
         }
     }
 
     const startDeletingEvent = () => {
-        
 
 
-        dispatch( onDeleteEvent() );
+
+        dispatch(onDeleteEvent());
     }
 
 

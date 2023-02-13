93428d285b12664ea7d41adcdb8f003b686e475a
Mariana Guzmán <mggzmn91@gmail.com>
2023-02-13 12:09:09 -0600

    Pruebas en uiSlice

---
+++ tests/store/ui/uiSlice.test.js
@@ -0,0 +1,12 @@
+import { onOpenDateModal, uiSlice } from "../../../src/store/ui/uiSlice";
+
+describe('Pruebas en uiSlice', () => {
+    test('debe de regresar el estado por defecto', () => {
+        expect(uiSlice.getInitialState().isDateModalOpen).toBeFalsy();
+    });
+    test('debe de cambiar el isDateModalOpen correctamente',()=>{
+        let state = uiSlice.getInitialState();
+        state = uiSlice.reducer(state, onOpenDateModal());
+        expect(state.isDateModalOpen).toBeTruthy();
+    });
+});

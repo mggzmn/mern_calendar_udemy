c1d83dd72c1ec5c36ae78f3cc2b567de2df543fc
Mariana Guzmán <mggzmn91@gmail.com>
2023-02-13 13:03:46 -0600

    Pruebas en authSlice actions and states

--- tests/store/auth/authSlice.test.js
+++ tests/store/auth/authSlice.test.js
@@ -1,9 +1,41 @@
-import { authSlice } from "../../../src/store/auth/authSlice";
-import { initialState } from "../../fixtures/authStates";
+import { authSlice, clearErrorMessage, onLogin, onLogout } from "../../../src/store/auth/authSlice";
+import { authenticatedState, initialState } from "../../fixtures/authStates";
+import { testUserCredentials } from "../../fixtures/testUser";
 
 describe('Pruebas en authSlice', () => {
     test('debe de regresar el estado por defecto', () => {
         expect(authSlice.getInitialState()).toEqual(initialState)
+    });
+    test('debe de realizar un login', () => {
+        const state = authSlice.reducer(initialState, onLogin(testUserCredentials));
+        expect(state).toEqual({
+            status: 'authenticated',
+            user: testUserCredentials,
+            errorMessage: undefined
+        })
+    });
+    test('debe de realizar un logout', () => {
+        const state = authSlice.reducer(authenticatedState, onLogout());
+        expect(state).toEqual({
+            status: 'not-authenticated',
+            user: {},
+            errorMessage: undefined
+        })
+    });
+    test('debe de realizar un logout', () => {
+        const errorMessage = "Credenciales no válidas"
+        const state = authSlice.reducer(authenticatedState, onLogout(errorMessage));
+        expect(state).toEqual({
+            status: 'not-authenticated',
+            user: {},
+            errorMessage: errorMessage
+        })
+    });
+    test('debe de limpiar mensaje de error', () => {
+        const errorMessage = "Credenciales no válidas"
+        const state = authSlice.reducer(authenticatedState, onLogout(errorMessage));
+        const newState = authSlice.reducer(state, clearErrorMessage())
+        expect(newState.errorMessage).toBe(undefined)
     });
 
 });
